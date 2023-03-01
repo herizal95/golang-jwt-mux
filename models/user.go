d173957c885caaf5f462aae820dabbf3e18eb64a
herizal95 <alphacsoft.herizal@gmail.com>
2023-03-01 12:18:15 +0800

    successfully connect to database

---
+++ models/user.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"github.com/satori/uuid"
+)
+
+type User struct {
+	Uid       uuid.UUID `gorm:"primary_key" json:"uid"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdateAt  time.Time `json:"updated_at"`
+}
